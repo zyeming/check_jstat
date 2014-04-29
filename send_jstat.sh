1b9898f40e7161a3e1e146fdc65f4eabdd96bdc1
Yeming <yeming@kanchufang.com>
2014-04-29 13:10:35 +0800

    update the data sent to nagios

--- send_jstat.sh
+++ send_jstat.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-bash /usr/local/nagios/libexec/check_jstat.sh -j Netty -w 60 -c 80 | /usr/local/nagios/bin/send_nsca -H 10.232.18.144 -p 5667 -d ";" -to 10 -c /usr/local/nagios/etc/send_nsca.cfg
+/bin/bash /usr/local/nagios/libexec/check_jstat.sh -j Netty -w 60 -c 80 | /usr/local/nagios/bin/send_nsca -H 10.232.18.144 -p 5667 -d ";" -to 10 -c /usr/local/nagios/etc/send_nsca.cfg
